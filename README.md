668a6b2e6a58c953a6ebb47c72acfccb6e7bc948
Yaroslav <yaroslav@storozhenko.dev>
2023-10-06 22:15:25 +0000

    fixed script lol

--- README.md
+++ README.md
@@ -2,25 +2,25 @@
 
 ## Overview
 
-This Bash script is designed to help you identify which Docker container ports are exposed to the internet. It's a useful tool for self-auditing, especially if you're managing multiple containers. The goal is to enhance your security posture by making sure you're not accidentally exposing more than you intend to.
+This Bash script helps you quickly identify which Docker container ports are exposed and which are internal. It's a handy tool for self-checking, especially when you're running multiple containers. The aim is to boost your security by ensuring you're not unintentionally exposing more than you should.
 
 ## How to Use 🚀
 
 1. Download the script.
-2. Make it executable with `chmod +x script.sh`.
-3. Run the script, passing your external IP as an argument: `./script.sh 1.2.3.4`.
+2. Run the script: `./docker_ports.sh`.
+
 
 ## Output 📊
 
 - Ports that are exposed to the internet will be displayed in **red**.
-- Ports that are not exposed will appear in **green**.
+- Ports that are internal-only will appear in **green**.
 
 ## Why You Need This 🤔
 
-Managing Docker containers often involves juggling various port mappings. It's easy to inadvertently expose a port that should remain internal. This script provides a quick way to review your current exposure, helping you catch any potential security risks before they become actual problems.
+Managing Docker containers can get complex, and it's easy to accidentally expose a port you didn't intend to. This script offers a quick and easy way to review your port exposure, helping you identify any potential security risks.
 
-## Disclamer
+## Disclaimer
 
-I am a lame Linux user, so this script must suck. Feel free to shoot a PR.
+Yeah, it's just a fancy output of `docker ps --format '{{.Names}}\t{{.Ports}}'`
 
 Stay secure! 🛡️
--- docker_ports.sh
+++ docker_ports.sh
@@ -1,43 +1,52 @@
 #!/bin/bash
 
-# Get external IP from input
-external_ip=$1
-
-# Validate IP
-if [[ -z "$external_ip" ]]; then
-  echo "Usage: $0 <external_ip>"
-  exit 1
-fi
-
 # ANSI color codes
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# Initialize output variables
+# Initialize output variables and max length
 exposed_output=""
 not_exposed_output=""
+max_length=0
 
-# Loop through Docker containers
-for container in $(docker ps --format '{{.Names}}'); do
-  # Get port mappings
-  mappings=$(docker inspect --format='{{range $p, $conf := .NetworkSettings.Ports}}{{if $conf}}{{$p}} -> {{(index $conf 0).HostPort}}{{"\n"}}{{end}}{{end}}' $container)
+# Get Docker container info
+docker_output=$(docker ps --format '{{.Names}}\t{{.Ports}}')
 
-  # Check each mapping
-  while read -r mapping; do
-    if [ -z "$mapping" ]; then continue; fi
+# First pass to find the longest container name
+while read -r line; do
+  container=$(echo -e "$line" | awk '{print $1}')
+  length=${#container}
+  if (( length > max_length )); then
+    max_length=$length
+  fi
+done <<< "$docker_output"
 
-    internal=$(echo $mapping | cut -d/ -f1)
-    external=$(echo $mapping | awk -F' -> ' '{print $2}')
+# Second pass to generate the output
+while read -r line; do
+  container=$(echo -e "$line" | awk '{print $1}')
+  ports=$(echo -e "$line" | cut -f2-)
 
-    # Check port
-    if nc -z -w2 $external_ip $external 2>/dev/null; then
-      exposed_output+="${RED}$container: $internal -> $external${NC}\n"
-    else
-      not_exposed_output+="${GREEN}$container: $internal -> $external${NC}\n"
-    fi
-  done <<< "$mappings"
-done
+  # Skip containers with no ports or no mappings
+  if [ -z "$ports" ] || [[ "$ports" == *"<->"* ]] || [[ ! "$ports" == *'->'* ]]; then
+    continue
+  fi
+
+  # Remove IP prefixes and trailing comma
+  ports=$(echo "$ports" | sed 's/0.0.0.0://g' | sed 's/127.0.0.1://g')
+
+  ports=$(echo "$ports" | sed 's/,$//')
+
+  # Align output
+  padding=$(printf "%*s" $((max_length - ${#container})) "")
+
+  # Check if ports are exposed
+  if [[ "$line" == *"0.0.0.0:"* ]] || [[ "$line" == *":::"* ]]; then
+    exposed_output+="${RED}${container}${padding}   ${ports}${NC}\n"
+  else
+    not_exposed_output+="${GREEN}${container}${padding}   ${ports}${NC}\n"
+  fi
+done <<< "$docker_output"
 
 # Sort and print output
 echo -e "$exposed_output"
